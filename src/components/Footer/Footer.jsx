917e3d97dfba29a9468e5adde1295d430fa06efa
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-01-19 22:33:37 +0300

    feat(footer): Создал компонент footer
    meta: task-3

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,15 @@ import './Footer.css';
 export function Footer() {
   return (
     <div className="Footer">
-      Я есть Footer
+      <p className="Footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="Footer__border"></div>
+      <nav className="Footer__nav">
+        <p className="Footer__copy">&copy;{new Date().getFullYear()}</p>
+        <div className="Footer__links">
+          <a href="https://practicum.yandex.ru" className="Footer__link" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+          <a href="https://github.com/ChernoSlava" className="Footer__link Footer__last-link" target="_blank" rel="noreferrer">Github</a>
+        </div>
+      </nav>
     </div>
   );
 };
