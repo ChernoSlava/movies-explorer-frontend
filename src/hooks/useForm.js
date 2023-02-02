5df9a2a0ca5fb2fc8e452ec57b3f6996f6f8a63c
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-02-02 17:23:43 +0300

    fix(review): Поле Почта теперь проверяет доп валидатор
    meta: Reg&Auth 5

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,5 +1,7 @@
 import { useState, useCallback } from "react";
 
+import isEmail from 'validator/es/lib/isEmail';
+
 export function useForm() {
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
@@ -10,6 +12,12 @@ export function useForm() {
     const input = e.target;
     const { name, value } = input;
 
+    if (name === 'email') {
+      if (!isEmail(value)) {
+          input.setCustomValidity('Указана некорректная почта');
+      }
+    }
+    
     setValues({ ...values, [name]: value });
     setErrors({ ...errors, [name]: input.validationMessage });
   };
