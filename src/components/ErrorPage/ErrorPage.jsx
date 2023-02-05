d683f5672b77c0bfb1fa87cd7328930c79c45550
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2023-02-05 16:10:51 +0300

    refactor(styled-components):
    Перевёл в styled Footer
    + fix: Удалил стили для errorPages
    
    meta: styled-components

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import './ErrorPage.css';
-
 import { ErrorPageStyled, ErrorPageBtn } from './styled';
 
 export function ErrorPage() {
